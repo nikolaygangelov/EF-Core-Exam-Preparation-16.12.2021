327b3656282c9a06061658e3d5235ffd32f330cd
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-12-29 19:42:06 +0200

    Update Deserializer.cs

--- EF Core Exam Preparation 16.12.2021/Artillery/DataProcessor/Deserializer.cs
+++ EF Core Exam Preparation 16.12.2021/Artillery/DataProcessor/Deserializer.cs
@@ -24,26 +24,35 @@
 
         public static string ImportCountries(ArtilleryContext context, string xmlString)
         {
+            //using Data Transfer Object Class to map it with Countries
             var serializer = new XmlSerializer(typeof(ImportCountriesDTO[]), new XmlRootAttribute("Countries"));
+
+            //Deserialize method needs TextReader object to convert/map 
             using StringReader inputReader = new StringReader(xmlString);
             var countriesArrayDTOs = (ImportCountriesDTO[])serializer.Deserialize(inputReader);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
+
+            //creating List where all valid countries can be kept
             List<Country> countriesXML = new List<Country>();
 
             foreach (ImportCountriesDTO countryDTO in countriesArrayDTOs)
             {
-                Country countryToAdd = new Country
-                {
-                    CountryName = countryDTO.CountryName,
-                    ArmySize = countryDTO.ArmySize
-                };
-
+                //validating info for country from data
                 if (!IsValid(countryDTO))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
+
+                //creating a valid country
+                Country countryToAdd = new Country
+                {
+                    //using identical properties in order to map successfully
+                    CountryName = countryDTO.CountryName,
+                    ArmySize = countryDTO.ArmySize
+                };
 
                 countriesXML.Add(countryToAdd);
                 sb.AppendLine(string.Format(SuccessfulImportCountry, countryToAdd.CountryName,
@@ -52,37 +61,48 @@
 
             context.Countries.AddRange(countriesXML);
 
+            //actual importing info from data
             context.SaveChanges();
 
+            //using TrimEnd() to get rid of white spaces
             return sb.ToString().TrimEnd();
         }
 
         public static string ImportManufacturers(ArtilleryContext context, string xmlString)
         {
+            //using Data Transfer Object Class to map it with manifacturers
             var serializer = new XmlSerializer(typeof(ImportManifacturersDTO[]), new XmlRootAttribute("Manufacturers"));
+
+            //Deserialize method needs TextReader object to convert/map 
             using StringReader inputReader = new StringReader(xmlString);
             var manufacturersArrayDTOs = (ImportManifacturersDTO[])serializer.Deserialize(inputReader);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
+
+            //creating List where all valid manufacturers can be kept
             List<Manufacturer> manufacturersXML = new List<Manufacturer>();
 
             foreach (ImportManifacturersDTO manufacturerDTO in manufacturersArrayDTOs)
             {
-
+                //validating info for manufacturer from data
                 if (!IsValid(manufacturerDTO))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //checking for duplicates
                 if (manufacturersXML.Any(m => m.ManufacturerName == manufacturerDTO.ManufacturerName))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //creating a valid manufacturer
                 Manufacturer manifacturerToAdd = new Manufacturer
                 {
+                    //using identical properties in order to map successfully
                     ManufacturerName = manufacturerDTO.ManufacturerName,
                     Founded = manufacturerDTO.Founded
                 };
@@ -100,31 +120,41 @@
 
             context.Manufacturers.AddRange(manufacturersXML);
 
+            //actual importing info from data
             context.SaveChanges();
 
+            //using TrimEnd() to get rid of white spaces
             return sb.ToString().TrimEnd();
         }
 
         public static string ImportShells(ArtilleryContext context, string xmlString)
         {
+            //using Data Transfer Object Class to map it with shells
             var serializer = new XmlSerializer(typeof(ImportShellsDTO[]), new XmlRootAttribute("Shells"));
+
+            //Deserialize method needs TextReader object to convert/map 
             using StringReader inputReader = new StringReader(xmlString);
             var shellsArrayDTOs = (ImportShellsDTO[])serializer.Deserialize(inputReader);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
+
+            //creating List where all valid shells can be kept
             List<Shell> shellsXML = new List<Shell>();
 
             foreach (ImportShellsDTO shellDTO in shellsArrayDTOs)
             {
-
+                //validating info for shell from data
                 if (!IsValid(shellDTO))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //creating a valid shell
                 Shell shellToAdd = new Shell
                 {
+                    //using identical properties in order to map successfully
                     ShellWeight = shellDTO.ShellWeight,
                     Caliber = shellDTO.Caliber
                 };
@@ -136,29 +166,37 @@
 
             context.Shells.AddRange(shellsXML);
 
+            //actual importing info from data
             context.SaveChanges();
 
+            //using TrimEnd() to get rid of white spaces
             return sb.ToString().TrimEnd();
         }
 
         public static string ImportGuns(ArtilleryContext context, string jsonString)
         {
+            //using Data Transfer Object Class to map it with guns
             var gunsArray = JsonConvert.DeserializeObject<ImportGunsDTO[]>(jsonString);
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
+
+            //creating List where all valid guns can be kept
             List<Gun> gunsList = new List<Gun>();
 
             foreach (ImportGunsDTO gunDTO in gunsArray)
             {
-
+                //validating info for gun from data
                 if (!IsValid(gunDTO))
                 {
                     sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
+                //creating a valid gun
                 Gun gunToAdd = new Gun()
                 {
+                    //using identical properties in order to map successfully
                     ManufacturerId = gunDTO.ManufacturerId,
                     GunWeight = gunDTO.GunWeight,
                     BarrelLength = gunDTO.BarrelLength,
@@ -183,8 +221,11 @@
             }
 
             context.Guns.AddRange(gunsList);
+
+            //actual importing info from data
             context.SaveChanges();
 
+            //using TrimEnd() to get rid of white spaces
             return sb.ToString().TrimEnd();
         }
         private static bool IsValid(object obj)
