76b79dfd96022ac8768d8aa5e23a304c0ed4137e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-12-29 20:10:57 +0200

    Update Serializer.cs

--- EF Core Exam Preparation 16.12.2021/Artillery/DataProcessor/Serializer.cs
+++ EF Core Exam Preparation 16.12.2021/Artillery/DataProcessor/Serializer.cs
@@ -12,6 +12,8 @@ namespace Artillery.DataProcessor
     {
         public static string ExportShells(ArtilleryContext context, double shellWeight)
         {
+            //turning needed info about shells into a collection using anonymous object
+            //using less data
             var shellsAndGuns = context.Shells
                 .Where(s => s.ShellWeight > shellWeight)
                 .Select(s => new
@@ -33,24 +35,32 @@ namespace Artillery.DataProcessor
                 .OrderBy(s => s.ShellWeight)
                 .ToArray();
 
+            //Serialize method needs object to convert/map
+	        //adding Formatting for better reading 
             return JsonConvert.SerializeObject(shellsAndGuns, Formatting.Indented);
         }
 
         public static string ExportGuns(ArtilleryContext context, string manufacturer)
         {
+            //using Data Transfer Object Class to map it with guns
             XmlSerializer serializer = new XmlSerializer(typeof(ExportGunsDTO[]), new XmlRootAttribute("Guns"));
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
+            //"using" automatically closes opened connections
             using var writer = new StringWriter(sb);
 
             var xns = new XmlSerializerNamespaces();
+
+            //one way to display empty namespace in resulted file
             xns.Add(string.Empty, string.Empty);
 
             var gunsAndCountries = context.Guns
                 .Where(g => g.Manufacturer.ManufacturerName == manufacturer)
                 .Select(g => new ExportGunsDTO
                 {
+                    //using identical properties in order to map successfully
                     Manufacturer = g.Manufacturer.ManufacturerName,
                     GunType = g.GunType.ToString(),
                     GunWeight = g.GunWeight,
@@ -69,10 +79,14 @@ namespace Artillery.DataProcessor
                 .OrderBy(g => g.BarrelLength)
                 .ToArray();
 
+            //Serialize method needs file, TextReader object and namespace to convert/map
             serializer.Serialize(writer, gunsAndCountries, xns);
+
+            //explicitly closing connection in terms of reaching edge cases
             writer.Close();
 
-            return sb.ToString();
+            //using TrimEnd() to get rid of white spaces
+            return sb.ToString().TrimEnd();
         }
     }
 }
